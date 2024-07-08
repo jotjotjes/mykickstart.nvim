2fc5e2f18260d0271b74a6e4747fd491eff7042d
Jes <jessah@web.de>
2024-07-08 14:53:37 +0200

    Allow hidden buffers. Autosave markdown files.

--- lua/options.lua
+++ lua/options.lua
@@ -59,4 +59,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 8
 
+-- Change buffers without saveing
+vim.opt.hidden = true
+
+--Autosave markdown files
+vim.api.nvim_create_autocmd('BufLeave', { pattern = '*.md', command = 'silent! wall' })
+
 -- vim: ts=2 sts=2 sw=2 et
