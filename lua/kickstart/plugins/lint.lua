6b52984099bce61d132d40dc25157d210652f94d
Jes <jessah@web.de>
2024-07-17 13:13:28 +0200

    Disable markdownlint rule MD013 (Line length).

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -8,6 +8,12 @@ return {
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
       }
+      local markdownlint = require('lint').linters.markdownlint
+      markdownlint.args = {
+        '--disable',
+        'MD013',
+        '--', -- Required
+      }
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
